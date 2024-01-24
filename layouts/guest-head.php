f0d432934eefd60a1479be1feeec752f295f51dd
dc-coder369 <82113657+dc-coder369@users.noreply.github.com>
2024-01-24 16:52:51 +0530

    Add files via upload

--- layouts/guest-head.php
+++ layouts/guest-head.php
@@ -14,7 +14,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Pages / Login - NiceAdmin Bootstrap Template</title>
+  <title>GMRC Ops Connect</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
