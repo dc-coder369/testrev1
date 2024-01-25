f27810f1f2a223543e294d719b0215007961ad1d
dc-coder369 <dalvikcache369@gmail.com>
2024-01-25 15:50:37 +0530

    minor changes

--- layouts/header.php
+++ layouts/header.php
@@ -21,7 +21,7 @@ $checkBoxArr = [
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Core PHP</title>
+  <title>GMRC Ops Connect</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -57,7 +57,7 @@ $checkBoxArr = [
   <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
+      <a href="dashboard.php" class="logo d-flex align-items-center">
         <img src="assets/img/g-logo.png" alt="">
         <span class="d-none d-lg-block">GMRC Ops Connect</span>
       </a>
